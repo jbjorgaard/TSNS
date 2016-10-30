6d46c217be497f2de1bde247f10a33c86f2bd271
Jeremiah Bjorgaard <jeremiah.bjorgaard@gmail.com>
2016-10-29 21:56:50 -0500

    testing yml object functionality

--- js/console.js
+++ js/console.js
@@ -33,6 +33,10 @@ define(['jquery', 'yaml', 'game'], function($, yaml, Game) {
 	    reader.onload = function(e) {
 		  var doc = yaml.safeLoad(reader.result, 'utf8');
 			console.log(doc);
+			console.log(doc.game.gameID);
+			if(doc.hasOwnProperty("game")){
+				console.log("game");
+			}
 		};
 			
 		reader.readAsText(file);
