d0e9462323a0a6ae5a9efb2fefece1d496b31a15
Jeremiah Bjorgaard <jeremiah.bjorgaard@gmail.com>
2016-11-03 17:59:43 -0500

    working on localStorage

--- js/console.js
+++ js/console.js
@@ -22,6 +22,30 @@ define(['jquery', 'yaml', 'game'], function($, yaml, Game) {
       })
       .focus();
       
+      var hasStorage = function() {
+    	  if(typeof(Storage) !== "undefined") {
+    		  console.log("Browser supports localStorage");
+    		  return true;
+    	  } else {
+    		  console.log("Browser does not support localStorage");
+    		  return false;
+    	  }
+      };
+      
+      var hasGameStored = function() {
+    	  if(hasStorage() || localStorage.hasOwnProperty("tsns")) {
+    		  console.log(localStorage.tsns);
+    		  console.log(localStorage.tsns[0]);
+    		  // returns first character of tsns string
+    		  return true;
+    	  } else {
+    		  return false;
+    	  }
+      };
+      
+      
+      hasGameStored();
+      
       var fileInput = document.getElementById('fileInput');
 	  var fileDisplayArea = document.getElementById('fileDisplayArea');
 	
@@ -33,9 +57,12 @@ define(['jquery', 'yaml', 'game'], function($, yaml, Game) {
 	    reader.onload = function(e) {
 		  var doc = yaml.safeLoad(reader.result, 'utf8');
 			console.log(doc);
-			console.log(doc.tsns.gameID);
 			if(doc.hasOwnProperty("tsns")){
 				console.log("tsns");
+				if(hasStorage()) {
+					localStorage.tsns = doc;
+					console.log("Game file has been saved to localStorage");
+				}
 			}
 		};
 			
